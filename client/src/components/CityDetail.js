1d04a7fa4d1651f138fc8a6e9d83a599c9933364
Clare Buckley <clarebuckley1@hotmail.com>
2020-05-08 21:03:58 +0100

    fix select/unselected behaviour

--- client/src/components/CityDetail.js
+++ client/src/components/CityDetail.js
@@ -17,30 +17,21 @@ class CityDetail extends Component {
             radius: 10000,
             sightings: [],
             city: "[[the city I clicked on]]",
-            selectedAbduction: 0,
+            selectedAbduction: '-',
             selectedFullSummary: "placeholder"
         }
     }
 
     componentDidMount() {
-        this.getCityData()
-            .then(() => {
-                this.getFullSummary(this.state.selectedAbduction)
-                .then((fullSummary) => {
-                    this.setState({
-                        isLoading: false,
-                        selectedFullSummary: fullSummary
-                    })
-                })
-            })
+        this.getCityData(); 
     }
 
     getCityData = () => {
         return new Promise((resolve, reject) => {
         axios.get(this.urlPrefix + 'http://ufo-api.herokuapp.com/api/sightings/location/near?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&radius=' + this.state.radius)
             .then((res) => {
-                console.log(res.data.sightings[2].obj);
                 this.setState({
+                    isLoading: false,
                     sightings: res.data.sightings
                 })
                 return resolve();
@@ -53,6 +44,11 @@ class CityDetail extends Component {
     }
 
     handleAbductionSelected = (index) => {
+        if (index === this.state.selectedAbduction) {
+            this.setState({
+                selectedAbduction: '-'
+            })
+        } else {
         this.setState({
             isLoading: true
         })
@@ -62,7 +58,8 @@ class CityDetail extends Component {
                 selectedAbduction: index,
                 selectedFullSummary: fullSummary
             })
-        })
+            })
+            }
 
     }
 
@@ -79,7 +76,7 @@ class CityDetail extends Component {
         })
 
     }
-        ;
+        
     stripHtml = (html) => {
         var doc = new DOMParser().parseFromString(html, 'text/html');
         return doc.body.textContent || "";
@@ -92,29 +89,34 @@ class CityDetail extends Component {
         } else {
             return (
                 <div className="column CityDetail">
-                    <p>Click any result to see more. All details have been entered by users of <a href="http://www.nuforc.org/">this site</a></p>
                     <div className="cityAbductionsContainer">
                         <div className="cityAbductionsHeader">
                             Sightings in {this.state.city}
                         </div>
                         <div className="cityAbductionsContent">
                             {this.state.sightings.map((sighting, index) => (
-                                <div key={index} className="cityAbductionSighting" onClick={() => { this.handleAbductionSelected(index) }} >
+                                <div key={index} className={this.state.selectedAbduction === index ? 'selectedCityAbductionSighting' : 'cityAbductionSighting'} onClick={() => { this.handleAbductionSelected(index) }} >
                                     <p>{sighting.obj.date.split("T")[0]}: {sighting.obj.city.split(" ")[0]}</p>
                                     <p>{sighting.obj.shape}: {sighting.obj.summary.substring(0, 25)}...</p>
                                 </div>
                             ))}
                         </div>
                     </div>
-                    <div className="focusedAbductionContainer">
-                        <div className="cityAbductionsHeader">
-                            {this.state.sightings[this.state.selectedAbduction].obj.shape} - {this.state.sightings[this.state.selectedAbduction].obj.date.split("T")[0]}: {this.state.sightings[this.state.selectedAbduction].obj.city}
+
+                    {this.state.selectedAbduction !== '-' ? (
+                        <div className="focusedAbductionContainer">
+                            <div className="cityAbductionsHeader">
+                                {this.state.sightings[this.state.selectedAbduction].obj.shape} - {this.state.sightings[this.state.selectedAbduction].obj.date.split("T")[0]}: {this.state.sightings[this.state.selectedAbduction].obj.city}
+                            </div>
+                            <div className="focusedAbductionContent">
+                                <p style={{ 'fontStyle': 'italic' }}>"{this.state.sightings[this.state.selectedAbduction].obj.summary}"</p>
+                                <p>{this.state.selectedFullSummary}</p>
+                            </div>
                         </div>
-                        <div className="focusedAbductionContent">
-                            <p style={{ 'fontStyle': 'italic' }}>"{this.state.sightings[this.state.selectedAbduction].obj.summary}"</p>
-                            <p>{this.state.selectedFullSummary}</p>
-                        </div>
-                    </div>
+                    ) : (
+                            <p>Click any result to see more. All details have been entered by users of <a href="http://www.nuforc.org/">this site</a></p>
+                        )}
+                    
                 </div>
             );
         }
