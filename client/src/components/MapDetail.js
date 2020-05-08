6c7f23ea88af4b060b4828b765d24b852036ef62
Clare Buckley <clarebuckley1@hotmail.com>
2020-05-08 15:16:06 +0100

    first request

--- client/src/components/MapDetail.js
+++ client/src/components/MapDetail.js
@@ -3,6 +3,33 @@ import React, { Component } from 'react';
 /** Responsible for rendering the map displaying probability of getting abducted
  * */
 class MapDetail extends Component {
+    constructor() {
+        super();
+        this.state = {
+            latitude: 51.18291,
+            longitude: -0.63098,
+            radius: 5000
+        }
+    }
+
+    componentDidMount() {
+        this.getCityData();
+    }
+
+    getCityData = () => {
+        console.log(this.state);
+        fetch('http://ufo-api.herokuapp.com/api/sightings/location/near?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&radius= ' + this.state.radius, {
+            mode: 'no-cors' // 'cors' by default
+        })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.error(err)
+            });
+    }
+
+
     render() {
         return (
             <div className="column">
