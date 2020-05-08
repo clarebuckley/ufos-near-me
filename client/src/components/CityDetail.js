9c6b5ccdd8d1eef17468797963a025736cf633c8
Clare Buckley <clarebuckley1@hotmail.com>
2020-05-08 18:22:22 +0100

    styling changes

--- client/src/components/CityDetail.js
+++ client/src/components/CityDetail.js
@@ -11,9 +11,10 @@ class CityDetail extends Component {
 
         //lat/long/city should come from map data
         this.state = {
+            isLoading: true,
             latitude: 51.18291,
             longitude: -0.63098,
-            radius: 5000,
+            radius: 50000,
             sightings: [],
             city: "[[the city I clicked on]]"
         }
@@ -27,6 +28,10 @@ class CityDetail extends Component {
         axios.get(this.urlPrefix + '/sightings/location/near?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&radius=' + this.state.radius)
             .then((res) => {
                 console.log(res.data.sightings[0]);
+                this.setState({
+                    isLoading: false,
+                    sightings: res.data.sightings
+                })
             })
             .catch((err) => {
                 console.error(err)
@@ -35,17 +40,24 @@ class CityDetail extends Component {
 
 
     render() {
-        return (
-            <div className="column">
-                <div className="cityAbductionsContainer">
-                    <div className="cityAbductionsHeader">
-                        Sightings in {this.state.city}
-                    </div>
-                    <div className="cityAbductionsContent">
+        if (this.state.isLoading) {
+            return "Loading...";
+        } else {
+            return (
+                <div className="column">
+                    <div className="cityAbductionsContainer">
+                        <div className="cityAbductionsHeader">
+                            Sightings in {this.state.city}
+                        </div>
+                        <div className="cityAbductionsContent">
+                            {this.state.sightings.map((sighting, index) => (
+                                <div className="cityAbductionSighting"> {sighting.obj.city} </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
