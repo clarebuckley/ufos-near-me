541de6acdaa497913221f5c0b6f9895dfd7a4586
Clare Buckley <clarebuckley1@hotmail.com>
2020-05-08 21:32:28 +0100

    fix a couple of console errors

--- client/src/components/MapDetail.js
+++ client/src/components/MapDetail.js
@@ -6,15 +6,21 @@ import RealMap from './RealMap'
 /** Responsible for rendering the map displaying probability of getting abducted
  * */
 class MapDetail extends Component {
-    state = {
-        incidents: [],
+    constructor() {
+        super();
+        this.state = {
+            incidents: [],
+            sightings: []
+        }
     }
 
     async componentDidMount() {
-        const res = await axios.get('/api/sightings/search?from=200/5/5&to=2016/8/7&shape=triangle&city=new%20york&state=ny&limit=50&skip=0', {
-        })
-        const sightings = res.data;
-        this.setState({ sightings: sightings });
+        axios.get('https://cors-anywhere.herokuapp.com/http://ufo-api.herokuapp.com/api/sightings/search?from=200/5/5&to=2016/8/7&shape=triangle&city=new%20york&state=ny&limit=50&skip=0')
+            .then((res) => {
+                const sightings = res.data;
+                this.setState({ sightings: sightings });
+            })
+        
     };
 
 
--- client/src/components/RealMap.js
+++ client/src/components/RealMap.js
@@ -3,38 +3,43 @@ import { TileLayer, Marker, Popup } from 'react-leaflet'
 import MapDetail from './MapDetail'
 
 export default class RealMap extends Component {
-  state = {
-    lat: 37.7749,
-    lng: -122.4194,
-    zoom: 13,
-  }
-
-  render() {
-    return (
-      this.props.sightings ?
-        <MapDetail
-          center={[this.state.lat, this.state.lng]}
-          zoom={this.state.zoom}
-          style={{ width: '100%', height: '900px' }}
-        >
-
-          {
-            this.props.sightings.map(sightings => {
-              const point = [sightings['loc']['0'][0], sightings['loc']['1'][0]]
+    constructor() {
+        super();
+        this.state = {
+            lat: 37.7749,
+            lng: -122.4194,
+            zoom: 13,
+        }
+    }
 
 
-              return (
-                <Marker position={point} key={sightings['_id']} >
-                  <Popup>
-                    <span>ADDRESS: {sightings['city']}, {sightings['state']}}</span>
-                  </Popup>
-                </Marker>
-              )
-            })
-          }
-        </MapDetail>
-        :
-        'Data is loading...'
-    )
-  }
+    render() {
+        if (!this.props.sightings) {
+            return "Data is loading..."
+        } else {
+            return (
+                <div className="RealMap">
+                    <MapDetail
+                        center={[this.state.lat, this.state.lng]}
+                        zoom={this.state.zoom}
+                        style={{ width: '100%', height: '900px' }}
+                    >
+                        {
+                            this.props.sightings.map(sightings => {
+                                const point = [sightings['loc']['0'][0], sightings['loc']['1'][0]]
+
+                                return (
+                                    <Marker position={point} key={sightings['_id']} >
+                                        <Popup>
+                                            <span>ADDRESS: {sightings['city']}, {sightings['state']}}</span>
+                                        </Popup>
+                                    </Marker>
+                                )
+                            })
+                        }
+                    </MapDetail>
+                </div>
+            )
+        }
+    }
 }
