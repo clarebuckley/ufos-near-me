97da14d37ccb8a4f6f8ec65c9cda44763ca08c9d
Jenny <jenniferrosemyers@gmail.com>
2020-05-08 21:04:32 +0100

    attempt at stuff does it work ? i dont think so but maybe clare will help@

--- client/src/components/MapDetail.js
+++ client/src/components/MapDetail.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import RealMap from './components/RealMap'
+
 
 /** Responsible for rendering the map displaying probability of getting abducted
  * */
 class MapDetail extends Component {
+    state = {
+        incidents: [],
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('/api/sightings/search?from=200/5/5&to=2016/8/7&shape=triangle&city=new%20york&state=ny&limit=50&skip=0', {
+        })
+        const sightings = res.data;
+        this.setState({ sightings: sightings });
+    };
 
 
     render() {
         return (
-            <div className="column">
-                <p> whole map </p>
-            </div>
+            <RealMap incidents={this.state.sightings} />
         );
     }
 }
