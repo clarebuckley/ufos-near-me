83f11152c73b0ffca9ce5774f3088a867d6b8c94
Clare Buckley <clarebuckley1@hotmail.com>
2020-05-08 17:34:26 +0100

    was in the wrong file sorry

--- client/src/components/CityDetail.js
+++ client/src/components/CityDetail.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 /** Responsible for rendering the abduction information for each city once clicked on the map
  * */
 class CityDetail extends Component {
+    constructor() {
+        super();
+        this.urlPrefix = 'https://cors-anywhere.herokuapp.com/http://ufo-api.herokuapp.com/api';
+        this.state = {
+            latitude: 51.18291,
+            longitude: -0.63098,
+            radius: 5000,
+            sightings: []
+        }
+    }
+
+    componentDidMount() {
+        this.getCityData();
+    }
+
+    getCityData = () => {
+        axios.get(this.urlPrefix + '/sightings/location/near?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&radius=' + this.state.radius)
+            .then((res) => {
+                console.log(res.data.sightings[0]);
+            })
+            .catch((err) => {
+                console.error(err)
+            });
+    }
+
+
     render() {
         return (
             <div className="column">
-                <p> abduction detail goes here </p>
+                <p> abduction here </p>
             </div>
         );
     }
--- client/src/components/MapDetail.js
+++ client/src/components/MapDetail.js
@@ -1,38 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 /** Responsible for rendering the map displaying probability of getting abducted
  * */
 class MapDetail extends Component {
-    constructor() {
-        super();
-        this.state = {
-            latitude: 51.18291,
-            longitude: -0.63098,
-            radius: 50000
-        }
-    }
-
-    componentDidMount() {
-        this.getCityData();
-    }
-
-    getCityData = () => {
-        console.log(this.state);
-        axios.get('https://cors-anywhere.herokuapp.com/http://ufo-api.herokuapp.com/api/sightings/location/near?lat=51.18291&lon=-0.63098&radius=5000')
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => {
-                console.error(err)
-            });
-    }
 
 
     render() {
         return (
             <div className="column">
-                <p> whole map goes here </p>
+                <p> whole map </p>
             </div>
         );
     }
