cf133b647059fc9365ad36c5c3bd7a9e69b2adcc
Jenny <jenniferrosemyers@gmail.com>
2020-05-08 23:01:26 +0100

    map actually loads

--- client/src/components/MapDetail.js
+++ client/src/components/MapDetail.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import RealMap from './RealMap'
-
-
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 /** Responsible for rendering the map displaying probability of getting abducted
  * */
+
 class MapDetail extends Component {
     constructor() {
-        super();
+        super()
         this.state = {
-            incidents: [],
-            sightings: []
+          lat: 51.505,
+          lng: -0.09,
+          zoom: 10
         }
     }
 
-    async componentDidMount() {
-        axios.get('https://cors-anywhere.herokuapp.com/http://ufo-api.herokuapp.com/api/sightings/search?from=200/5/5&to=2016/8/7&shape=triangle&city=new%20york&state=ny&limit=50&skip=0')
-            .then((res) => {
-                const sightings = res.data;
-                this.setState({ sightings: sightings });
-            })
-        
-    };
-
-
-    render() {
-        return (
-            <RealMap sightings={this.state.sightings} />
-        );
-    }
+render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
 }
 
 export default MapDetail;
