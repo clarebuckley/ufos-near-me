322a151a5a6ef702d73c35c384548d6cf0f01555
Clare Buckley <clarebuckley1@hotmail.com>
2020-05-09 12:58:50 +0100

    a tiny map shows up now

--- client/src/components/MapDetail.js
+++ client/src/components/MapDetail.js
@@ -7,28 +7,28 @@ class MapDetail extends Component {
     constructor() {
         super()
         this.state = {
-          lat: 51.505,
-          lng: -0.09,
-          zoom: 10
+            lat: 51.505,
+            lng: -0.09,
+            zoom: 13
         }
     }
 
-render() {
-    const position = [this.state.lat, this.state.lng];
-    return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br/> Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
-    );
-  }
+    render() {
+        const position = [this.state.lat, this.state.lng];
+        return (
+            <Map id="MapDetail" className="column" center={position} zoom={this.state.zoom}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                </Marker>
+            </Map>
+        );
+    }
 }
 
 export default MapDetail;
